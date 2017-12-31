5585b0db8f7347063ac76ea4410d9072a6962a12
wkwksama <mailpakgasing@gmail.com>
2017-12-31 11:27:03 +0700

    implemen handlebar partial untuk template view

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs')
 
 var web = require('./routes/web');
 var users = require('./routes/users');
 
 var app = express();
+
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
