70b80f06d1aa23f0ac27f7919e3b4b4728310d5d
wkwksama <mailpakgasing@gmail.com>
2018-01-01 21:10:25 +0700

    auth login dengan jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const crypto = require("crypto");
 const shortid = require("shortid");
 
+var jwt = require("jsonwebtoken");
+
 let User = require("../models/user");
 
 function hashPassword(password, salt){
@@ -10,6 +12,7 @@ function hashPassword(password, salt){
   return value;
 };
 
+// hash password dengan salt baru yang di generate
 function saltHashPassword(userpassword) {
   const salt = shortid.generate();; /** Gives us salt of length 16 */
   const hash = hashPassword(userpassword, salt);
@@ -45,5 +48,50 @@ module.exports = {
         res.send(result);
       }
     });
+  },
+  login: function(req, res, next) {
+    if (req.body.username == "" || req.body.password == "") {
+      res.json({
+        success: false,
+        status: "ERROR",
+        message: "Username and Password can not empty"
+      });
+    } else {
+      User.findOne({ username: req.body.username }, function(err, user) {
+        if (err) {
+          console.log("Error when trying to login : ", err);
+
+          res.json({
+            success: false,
+            status: "ERROR",
+            message: err
+          });
+        } else if (!user) {
+          res.json({
+            success: false,
+            status: "ERROR",
+            message: "Authentication failed. User not found."
+          });
+        } else if (user) {
+          let reqPasswordData = hashPassword(req.body.password, user.salt);
+
+          if(user.password != reqPasswordData){
+            res.json({
+              success: false,
+              status: "ERROR",
+              message: "Authentication failed. Wrong password."
+            });
+          } else {
+             var token = jwt.sign({ user: user[0] }, 'rahasia');
+
+             res.json({
+               success: true,
+               msg: "login success",
+               token: token
+             });
+          }
+        }
+      })
+    }
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 
+router.post('/login', authController.login)
 router.post('/register', authController.register)
 
 module.exports = router;
